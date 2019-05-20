29d5d6b55afe4cca3b887e7160f098b5b68cb271
Matt Davis <matthewpeterdavis@gmail.com>
2019-05-20 10:41:55 +0300

    register service worker

--- client/src/index.js
+++ client/src/index.js
@@ -5,4 +5,4 @@ import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-serviceWorker.unregister();
+serviceWorker.register();
